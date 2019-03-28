d80c926ceb8cf170c53a6549effdc32ca7180b95
Tom Stanley <tom.stanley@alliescomputing.com>
2019-03-28 15:34:10 +0000

    Displaying 404 Error Routes using Switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
 	BrowserRouter,
-	Route
+	Route,
+	Switch
 } from 'react-router-dom';
 
 // App components
@@ -10,17 +11,20 @@ import About from "./About";
 import Header from "./Header";
 import Teachers from "./Teachers";
 import Courses from "./Courses";
+import NotFound from "./NotFound";
 
 const App = () => (
 	<BrowserRouter>
 	  <div className="container">
 			<Header />
-			{// 'Exact' prop here makes sure that the / is only used for the Index page and not included in other routes which also contain a /
-			}
-			<Route exact path="/" component={Home} />
-			<Route path="/about" render={ () => <About title="About"/> } />
-			<Route path="/teachers" component={Teachers} />
-			<Route path="/courses" component={Courses} />
+			<Switch> {/* Switch looks through all the routes until it finds one that works. If it can't find one, it'll render the last one. */}
+				{/* 'Exact' prop here makes sure that the / is only used for the Index page and not included in other routes which also contain a */}
+				<Route exact path="/" component={Home} />
+				<Route path="/about" render={ () => <About title="About"/> } />
+				<Route path="/teachers" component={Teachers} />
+				<Route path="/courses" component={Courses} />
+				<Route component={NotFound} />
+			</Switch>
 	  </div>
 	</BrowserRouter>
 );
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,6 +4,7 @@ const NotFound = () => (
   <div className="main-content not-found">
     <i className="material-icons icn-error">error_outline</i>
     <h2>Page Not Found</h2>
+    <p>Ah bollocks</p>
   </div>
 );
 
