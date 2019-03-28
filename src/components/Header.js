0fd36b33ea8796499c314c9020571091322dd129
Tom Stanley <tom.stanley@alliescomputing.com>
2019-03-28 14:41:00 +0000

    Styling Active Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
       <li>
-				<Link to="/">Home</Link>
+				{// activeStyle is a way of directly affecting the styles of an active NavLink
+				}
+				<NavLink exact to="/" activeStyle={{ background: "tomato"}}>Home</NavLink>
 			</li>
       <li>
-				<Link to="/about">About</Link>
+			{// activeClassName lets you customise the class that gets applied to active NavLinks, rather than having it be the default "active"
+			}
+				<NavLink to="/about" activeClassName="active i-am-active-man">About</NavLink>
 			</li>
       <li>
-				<Link to="/teachers">Teachers</Link>
+				<NavLink to="/teachers">Teachers</NavLink>
 			</li>
       <li>
-				<Link to="/courses">Courses</Link>
+				<NavLink to="/courses">Courses</NavLink>
 			</li>
     </ul>
   </header>
