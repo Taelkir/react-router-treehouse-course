8aea09eb5548a7fea8d0b01156265c54501be68a
Tom Stanley <tom.stanley@alliescomputing.com>
2019-03-28 14:11:09 +0000

    Installing React Router and Declaring Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {
+	BrowserRouter,
+	Route
+} from 'react-router-dom';
+
+// App components
+import Home from "./Home";
+import About from "./About";
+import Header from "./Header";
+import Teachers from "./Teachers";
+import Courses from "./Courses";
 
 const App = () => (
-  <div className="container">
-
-  </div>
+	<BrowserRouter>
+	  <div className="container">
+			<Header />
+			{// 'Exact' attribute here makes sure that the / is only used for the Index page and not included in other routes which also contain a /
+			}
+			<Route exact path="/" component={Home} />
+			<Route path="/about" component={About} />
+			<Route path="/teachers" component={Teachers} />
+			<Route path="/courses" component={Courses} />
+	  </div>
+	</BrowserRouter>
 );
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
-class Home extends Component {    
-  
+class Home extends Component {
+
   render() {
     return (
       <div className="main-content home">
         <h2>Front End Course Directory</h2>
         <p>This fun directory is a project for the <em>React Router Basics</em> course on Treehouse.</p>
-        <p>Learn front end web development and much more! This simple directory app offers a preview of our course library. Choose from many hours of content, from HTML to CSS to JavaScript. Learn to code and get the skills you need to launch a new career in front end web development.</p>
-        <p>We have thousands of videos created by expert teachers on web design and front end development. Our library is continually refreshed with the latest on web technology so you will never fall behind.</p>
+        <p>You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager? Life finds a way. We gotta burn the rain forest, dump toxic waste, pollute the air, and rip up the OZONE! 'Cause maybe if we screw up this planet enough, they won't want it anymore!</p>
+        <p>Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Is this my espresso machine? Wh-what is-h-how did you get my espresso machine? Hey, take a look at the earthlings. Goodbye! What do they got in there? King Kong?</p>
+
         <hr />
-        
+
       </div>
     );
   }
